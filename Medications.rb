6f8b868ac94ff3c0fda4c0aba64f4f8d7841c296
Cathy93 <CathyMarion93@gmail.com>
2017-03-05 18:45:49 +1100

    Medications test

---
+++ Medications.rb
@@ -0,0 +1,3 @@
+
+
+drugs = { :ventolin => {:name => 'Ventolin', :dose => '100mg', :dose_times => ['0800', '1200', '1600', '2000']} }
--- nurse.rb
+++ nurse.rb
@@ -1,6 +1,8 @@
 require 'csv'
 require 'terminal-table'
 require 'Date'
+require_relative 'medications'
+@drugs = { :ventolin => {:name => 'Ventolin', :dose => '100mg', :dose_times => ['0800', '1200', '1600', '2000']} }
 
 def open_csv(filename)
   patients = []
@@ -14,7 +16,7 @@ def open_csv(filename)
       dob = row['dob']
       age = age(dob)
       current_diagnosis = row['current_diagnosis']
-      patient = { :bed_no => bed_no, :hospital_no => hospital_no, :first_name => first_name, :last_name => last_name, :dob => dob, :age => age, :current_diagnosis => current_diagnosis}
+      patient = { :bed_no => bed_no, :hospital_no => hospital_no, :first_name => first_name, :last_name => last_name, :dob => dob, :age => age, :current_diagnosis => current_diagnosis, :medications => @drugs[:ventolin]}
       patients.push(patient)
   end
 
@@ -57,11 +59,14 @@ def build_table(patients)
   rows = []
   patients.each do | person |
     rows << [person[:bed_no], person[:hospital_no], person[:first_name], person[:last_name], person [:dob], person[:age], person[:current_diagnosis] ]
+    x = person[:medications]
+    puts x[:name]
   end
+
 
   table = Terminal::Table.new :rows => rows
   table.title = "Your Patients for the Day!"
-  table.headings = ['Bed Number', 'Hospital Number', 'First Name', 'Last Name', 'D.O.B', 'Age', 'Current Diagnosis']
+  table.headings = ['Bed Number', 'Hospital Number', 'First Name', 'Last Name', 'D.O.B', 'Age', 'Current Diagnosis', 'Medications']
   puts table
 end
 
